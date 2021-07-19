041df86773f02ee070aa41d8f018db501e23da12
infosergio2020 <infosergio2020@gmail.com>
2021-07-19 17:55:45 -0300

    se actualizo la forma en la que se despliega el sidebar haci como el desplazamiento de retorno al inicio

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-   
    let btnSide = document.querySelector("#btn-side");
-   btnSide.addEventListener('click',saludo);
+   btnSide.addEventListener('click',hide);
+
+   window.addEventListener('click', function(e){
+        let btnSide = document.querySelector("#btn-side");
+        let imgSide = document.querySelector("#img-side");
+        if (e.target.nodeName != 'A' && e.target != imgSide && e.target != btnSide){
+            // console.log('before: ',document.querySelector("#sidebar").classList.value);
+            if(!sidebar.classList.contains('hide')){
+                sidebar.classList.add('hide')
+            }
+            // console.log('after: ',document.querySelector("#sidebar").classList.value);
+        } 
+    })
 });
-
-
-function show() {
-    let sidebar = document.querySelector("#sidebar");
-    sidebar.classList.toggle("show");
-}
 
 function hide() {
     let sidebar = document.querySelector("#sidebar");
-    sidebar.classList.toggle("show");
+    // sidebar.classList.toggle("hide");
+    if(sidebar.classList.contains('hide')){
+        sidebar.classList.remove('hide');
+    } else {
+        sidebar.classList.add('hide');
+    }
 }
